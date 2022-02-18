dfdabc87d5d781ec556def061b038463b773cfe4
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2022-02-18 21:30:30 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Dogukan Gundogan on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
